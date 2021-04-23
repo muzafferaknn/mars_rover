b5467173c28b6b7b0dcda6644d42de60420392e9
Muzaffer Akın <45818462+muzafferaknn@users.noreply.github.com>
2021-04-23 16:34:59 +0300

    first commit

---
+++ Calculate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mars_Rover
+{
+    public class Coordinates
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    public class RoverPosition : Coordinates
+    {
+        public EnumDirection Direction { get; set; }
+
+        public RoverPosition()
+        {
+            X = 0;
+            Y = 0;
+            Direction = EnumDirection.N;
+        }
+    }
+    public enum EnumDirection
+    {
+        N,
+        S,
+        E,
+        W
+    }
+
+    public static class Calculate
+    {
+        private static RoverPosition MoveForward(RoverPosition position)
+        {
+            switch (position.Direction)
+            {
+                case EnumDirection.N:
+                    position.Y += 1;
+                    break;
+                case EnumDirection.S:
+                    position.Y -= 1;
+                    break;
+                case EnumDirection.E:
+                    position.X += 1;
+                    break;
+                case EnumDirection.W:
+                    position.X -= 1;
+                    break;
+                default:
+                    break;
+            }
+            return position;
+        }
+        private static RoverPosition RotateL(RoverPosition position)
+        {
+            switch (position.Direction)
+            {
+                case EnumDirection.N:
+                    position.Direction = EnumDirection.W;
+                    break;
+                case EnumDirection.S:
+                    position.Direction = EnumDirection.E;
+                    break;
+                case EnumDirection.E:
+                    position.Direction = EnumDirection.N;
+                    break;
+                case EnumDirection.W:
+                    position.Direction = EnumDirection.S;
+                    break;
+                default:
+                    break;
+            }
+            return position;
+        }
+        private static RoverPosition RotateR(RoverPosition position)
+        {
+            switch (position.Direction)
+            {
+                case EnumDirection.N:
+                    position.Direction = EnumDirection.E;
+                    break;
+                case EnumDirection.S:
+                    position.Direction = EnumDirection.W;
+                    break;
+                case EnumDirection.E:
+                    position.Direction = EnumDirection.S;
+                    break;
+                case EnumDirection.W:
+                    position.Direction = EnumDirection.N;
+                    break;
+                default:
+                    break;
+            }
+            return position;
+        }
+        public static RoverPosition CalculateNewPosition(Coordinates _startPosition, RoverPosition roverPosition, string orientation)
+        {
+            foreach (var item in orientation)
+            {
+                switch (item)
+                {
+                    case 'L':
+                        roverPosition = RotateL(roverPosition);
+                        break;
+                    case 'R':
+                        roverPosition = RotateR(roverPosition);
+                        break;
+                    case 'M':
+                        roverPosition = MoveForward(roverPosition);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong character");
+                        break;
+                }
+            }
+            return roverPosition;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mars_Rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinates upper_right = new Coordinates();
+            Console.Write("Enter upper-right coordinates : ");
+            string start_position = Console.ReadLine();
+            if (start_position.Split(' ').Length == 2)
+            {
+                upper_right.X = Convert.ToInt32(start_position.Split(' ')[0]);
+                upper_right.Y = Convert.ToInt32(start_position.Split(' ')[1]);
+            }
+            else
+            {
+                Console.WriteLine("Wrong coordinates");
+                return;
+            }
+            Console.WriteLine("--------------------- ~ ---------------------");
+            while (true)
+            {
+                Console.Write("Enter rover coordination and position : ");
+                string rover = Console.ReadLine();
+                Console.Write("Enter rover's orientation : ");
+                string orientation = Console.ReadLine();
+                if (rover.Split(' ').Length == 3)
+                {
+                    RoverPosition position = new RoverPosition();
+                    position.X = Convert.ToInt32(rover.Split(' ')[0]);
+                    position.Y = Convert.ToInt32(rover.Split(' ')[1]);
+                    position.Direction = (EnumDirection)Enum.Parse(typeof(EnumDirection), rover.Split(' ')[2].ToUpper());
+
+
+                    position = Calculate.CalculateNewPosition(upper_right, position, orientation.ToUpper());
+
+                    if (position.X < 0 || position.Y < 0 || position.X > upper_right.X || position.Y > upper_right.Y)
+                        Console.WriteLine($"Rover position must be between (0,0) and ({upper_right.X},{upper_right.Y})");
+                    else
+                        Console.WriteLine($"New coordination and position : ({position.X},{position.Y}) {position.Direction}");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong coordination and position");
+                    return;
+                }
+                Console.WriteLine("--------------------- ~ ---------------------");
+
+            }
+        }
+    }
+}
